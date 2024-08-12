6934f5846581b22a4023ddd0c266ef3abbced58e
dunkvippro <118897462+dunkvippro@users.noreply.github.com>
2024-08-12 15:13:03 +0700

    init

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import App from './App'
 import './index.css'
 import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react'
-import { rootStore, persist } from './redux/config.tsx'
+import { rootStore, persist } from './redux/config'
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <Provider store={rootStore}>
         <PersistGate persistor={persist}>
