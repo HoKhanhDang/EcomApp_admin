88431038390650ee88bffebe2cf22f30f0e05c19
HoKhanhDang <121717409+HoKhanhDang@users.noreply.github.com>
2024-08-16 18:40:30 +0700

    Update Login.tsx

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -7,10 +7,13 @@ import swal from 'sweetalert'
 import { adminApi } from '../apis/adminApi'
 import { useNavigate } from 'react-router-dom'
 import { path } from '../ultils/path'
+//MUI
+import { LuLoader2 } from "react-icons/lu";
 
 const Login = () => {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
+    const [isLoading, setIsLoading] = useState(false)
     const dispatch = useDispatch()
     const navigate = useNavigate()
 
@@ -26,22 +29,26 @@ const Login = () => {
                 icon: 'warning'
             })
         } else {
+            setIsLoading(true)
             const rs = await adminApi.apiLogin(email, password)
             const { message } = rs
 
             if (message === 'Invalid email or password') {
                 swal('Login failed!', 'Invalid password', 'error')
                 setPassword('')
+                setIsLoading(false)
                 return
             } else if (message === 'Account do not exist') {
                 swal('Login failed!', 'Account do not exist', 'error')
                 setEmail('')
                 setPassword('')
+                setIsLoading(false)
                 return
             } else if (message === 'You are not authorized to login') {
                 swal('Login failed!', 'You are not an admin to login', 'error')
                 setEmail('')
                 setPassword('')
+                setIsLoading(false)
                 return
             }
             dispatch(
@@ -51,6 +58,7 @@ const Login = () => {
                     token: rs.accessToken
                 })
             )
+            setIsLoading(false)
             await swal('Login', 'Successfully!', 'success', {
                 timer: 2000,
                 buttons: false
@@ -60,46 +68,51 @@ const Login = () => {
         }
     }
     return (
-        <div className='w-screen h-screen flex items-center justify-center bg-[#F0F0F1]'>
-            <div className='font-main w-[60%] h-[80%] p-[30px] relative flex flex-row justify-start items-center shadow-2xl rounded-[50px] bg-white'>
-                <div className='w-1/2 flex justify-center items-center'>
-                    <img src={loginImage} className='w-[350px] h-[350px]' alt='' />
+        <>
+            {isLoading && (
+                <div className="w-screen h-screen z-50 absolute bg-blue-200 flex justify-center items-center">
+                    <LuLoader2
+                        variant="soft"
+                        className="animate-spin text-[40px]"
+                    />
                 </div>
-                <div className='flex w-1/2 px-[100px] flex-col justify-start items-center'>
-                    <span className='text-[45px] font-semibold my-5'>Login</span>
-                    <span className='text-[20px]'>Welcome Back!</span>
-                    <div className='w-full flex flex-col justify-start items-center my-1 gap-1'>
-                        <span className='w-full font-main text-[15px]'>Email</span>
-                        <InputField
-                            type={'email'}
-                            value={email}
-                            onChange={handleEmailChange}
-                            placeholder={'Enter the email'}
-                        />
+            )}
+            <div className='w-screen h-screen flex items-center justify-center bg-[#F0F0F1]'>
+                <div className='font-main w-[60%] h-[80%] p-[30px] relative flex flex-row justify-start items-center shadow-2xl rounded-[50px] bg-white'>
+                    <div className='w-1/2 flex justify-center items-center'>
+                        <img src={loginImage} className='w-[350px] h-[350px]' alt='' />
                     </div>
-                    <div className='w-full flex flex-col justify-start items-center my-1 gap-1'>
-                        <span className='w-full font-main text-[15px]'>Password</span>
-                        <InputField
-                            type={'password'}
-                            value={password}
-                            onChange={handlePasswordChange}
-                            placeholder={'Enter the password'}
-                        />
+                    <div className='flex w-1/2 px-[100px] flex-col justify-start items-center'>
+                        <span className='text-[45px] font-semibold my-5'>Login</span>
+                        <span className='text-[20px]'>Welcome Back!</span>
+                        <div className='w-full flex flex-col justify-start items-center my-1 gap-1'>
+                            <span className='w-full font-main text-[15px]'>Email</span>
+                            <InputField
+                                type={'email'}
+                                value={email}
+                                onChange={handleEmailChange}
+                                placeholder={'Enter the email'}
+                            />
+                        </div>
+                        <div className='w-full flex flex-col justify-start items-center my-1 gap-1'>
+                            <span className='w-full font-main text-[15px]'>Password</span>
+                            <InputField
+                                type={'password'}
+                                value={password}
+                                onChange={handlePasswordChange}
+                                placeholder={'Enter the password'}
+                            />
+                        </div>
+                        <button
+                            onClick={handleSubmit}
+                            className='bg-blue-500 hover:bg-blue-300 text-white p-3 my-3 rounded-md w-full'
+                        >
+                            Sign in
+                        </button>
                     </div>
-                    <button
-                        onClick={handleSubmit}
-                        className='bg-blue-500 hover:bg-blue-300 text-white p-3 my-3 rounded-md w-full'
-                    >
-                        Sign in
-                    </button>
                 </div>
             </div>
-            <div className='fixed bottom-5 w-[300px] h-[150px] right-5 flex flex-col border border-green-500 justify-center items-center'>
-                <span>Test Account</span>
-                <span className='text-[15px]'>Email: usd96011@tccho.com </span>
-                <span className='text-[15px]'>Password: 12 </span>
-            </div>
-        </div>
+        </>
     )
 }
 
